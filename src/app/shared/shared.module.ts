00779ad923a90249f19742536d2b0e447b2a27ef
MarcelMerz <marcel.merz@web.de>
2020-02-28 09:19:35 +0100

    Removed unused import

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {HttpClientModule} from '@angular/common/http';
 import { BitcoinService } from './services/bitcoin.service';
-import { MarketcapComponent } from './components/marketcap/marketcap.component';
 import { IonicModule } from '@ionic/angular';
 import { BitcoinInfoCardComponent } from './components/bitcoin-info-card/bitcoin-info-card.component';
 
