902399d7ef48773d750765b1129757022d2c96c4
MarcelMerz <marcel.merz@web.de>
2020-02-28 09:03:21 +0100

    Used Bitcoin-Info-Card

--- src/app/bitcoin-details/bitcoin-details.page.ts
+++ src/app/bitcoin-details/bitcoin-details.page.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { BitcoinService } from '../shared/services/bitcoin.service';
+import InfoCardData from '../shared/models/info-card-data';
 
 @Component({
   selector: 'app-bitcoin-details',
   templateUrl: './bitcoin-details.page.html',
   styleUrls: ['./bitcoin-details.page.scss'],
 })
-export class BitcoinDetailsPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class BitcoinDetailsPage { 
+  constructor(private bcService: BitcoinService) { }
+  bitCoinInfoCards = [
+    {
+      title: 'Marktkapitalisierung',
+      action: this.bcService.getMarketcap()
+    } as InfoCardData,
+    {
+      title: 'Anzahl aller Bitcoins im Umlauf',
+      action: this.bcService.getTotalbc()
+    } as InfoCardData,
+    {
+      title: 'Anzahl der Transaktionen in den letzten 24h',
+      action: this.bcService.getDaytransactioncount()
+    } as InfoCardData,
+    {
+      title: 'Anzahl gesendeter Bitcoin in den letzten 24h',
+      action: this.bcService.getDaybtcsent()
+    } as InfoCardData,
+    {
+      title: 'Aktuelle Hashrate',
+      action: this.bcService.getHashrate()
+    } as InfoCardData,
+    {
+      title: 'Aktueller Schwierigkeitsgrad',
+      action: this.bcService.getDifficulty()
+    } as InfoCardData,
+  ];
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,14 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {HttpClientModule} from '@angular/common/http';
 import { BitcoinService } from './services/bitcoin.service';
+import { MarketcapComponent } from './components/marketcap/marketcap.component';
+import { IonicModule } from '@ionic/angular';
+import { BitcoinInfoCardComponent } from './components/bitcoin-info-card/bitcoin-info-card.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BitcoinInfoCardComponent
+  ],
+  exports: [
+    BitcoinInfoCardComponent
+  ],
   imports: [
     CommonModule,
-    HttpClientModule
+    HttpClientModule,
+    IonicModule
   ], 
   providers: [
     BitcoinService
